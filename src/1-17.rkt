8509198476b87e116768aeafc109bfc273867f5b
savarin <ezzeri.esa@gmail.com>
2022-12-13 08:31:11 -0800

    exercises - 1.17

---
+++ src/1-17.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+(define (mul a b)
+  (define (iter b result)
+    (if (= b 0)
+      result
+      (iter (- b 1) (+ a result))))
+
+  (iter b 0)
+)
--- src/1-30.rkt
+++ src/1-30.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
 (define (sum term a next b)
-    (define (iter a result)
-      (if (> a b)
-          result
-          (iter (next a) (+ (term a) result))))
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
 
-    (iter a 0))
+  (iter a 0))
