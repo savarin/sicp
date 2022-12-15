6b39b03d39c02994894f85f2ee33b66891101d45
savarin <ezzeri.esa@gmail.com>
2022-12-15 02:36:16 -0800

    exercises - 2.33, 2.38

---
+++ src/2-33.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence) (accumulate op initial (cdr sequence)))))
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (cons (p x) y)) null sequence))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+
+(define (filter proc sequence)
+  (define (attach x y)
+          (if (proc x)
+              (cons x y)
+               y))
+  (accumulate attach null sequence))
